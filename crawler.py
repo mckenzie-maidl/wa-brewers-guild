78603b21d720f10896ad567ec2c24e4c240324c8
david chen <yenting.chen03@gmail.com>
2024-04-26 13:28:38 -0700

    model prediction with added features

--- crawler.py
+++ crawler.py
@@ -2,9 +2,9 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import pandas as pd
 
-data = pd.read_excel('WA Breweries.xlsx')
-links = data['Untappd Profile Link'].values
-breweries = data['Brewery'].values
+data = pd.read_csv('data/wa_breweries.csv')
+links = data['untappd_profile_link'].values
+breweries = data['brewery'].values
 
 options = webdriver.ChromeOptions()
 options.add_argument(r"--user-data-dir=C:\Users\david.chen\AppData\Local\Google\Chrome\User Data")
@@ -12,7 +12,14 @@ options.add_argument(r'--profile-directory=Profile 1') #e.g. Profile 3
 options.add_argument(r'--executable-path=C:\Users\david.chen\Downloads\chromedriver_win32\chromedriver.exe')
 driver = webdriver.Chrome(options=options)
 
-ratings = {}
+def num_handler(s):
+    if type(s) == str:
+        s =s.replace(',', '')
+        if "M+" in s:
+            s = s.replace('M+', "000000")
+    return float(s)
+
+ratings = {}; totalVisits = {}; uniqueVisits = {}; monthlyVisits = {}
 for br, link in zip(breweries, links):
     try:
         driver.get(link)
@@ -21,16 +28,23 @@ for br, link in zip(breweries, links):
         elem = driver.find_element(By.XPATH, '//span[@class = "num"]')
         statsElem = driver.find_element(By.XPATH, '//div[@class = "stats"]')
         statsText = statsElem.text.split('\n')
-        totalVisits = int(''.join(statsText[1].split(',')))
-        uniqueVisits = int(''.join(statsText[3].split(',')))
-        monthlyVisits = int(''.join(statsText[5].split(',')))
-        stats = {'total': totalVisits, 'unique': uniqueVisits, 'monthly': monthlyVisits}
+        totalVisit = num_handler(statsText[1])
+        uniqueVisit = num_handler(statsText[3])
+        monthlyVisit = num_handler(statsText[5])
+        stats = {'total': totalVisit, 'unique': uniqueVisit, 'monthly': monthlyVisit}
         rating = float(elem.text[1:-1])
         ratings[br] = rating
+        totalVisits[br] = totalVisit
+        uniqueVisits[br] = uniqueVisit
+        monthlyVisits[br] = monthlyVisit
         stats['rating'] = rating
         print(stats)
     except:
         pass
 
-data['rating'] = data['Brewery'].map(ratings)
-data.to_excel('WA Breweries.xlsx', index = False)
+data['rating'] = data['brewery'].map(ratings)
+data['total_visits'] = data['brewery'].map(totalVisits)
+data['unique_visits'] = data['brewery'].map(uniqueVisits)
+data['monthly_visits'] = data['brewery'].map(monthlyVisits)
+data.to_csv('data/wa_breweries_with_ratings.csv', index = False)
+driver.quit()
